13c9d077e2a8f96fa45c8de1d7dcf02e624acf90
Anastasiya <zagoraai33@gmail.com>
2020-05-15 23:13:19 +0300

    15.05

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
     const mobileMenuToggle = document.querySelector('.toggle-menu');
     const mobMenu = document.querySelector('.header-menu');
+    const overlay = document.querySelector('#overlay')
+
     mobileMenuToggle.addEventListener('click', function(){
         mobMenu.classList.toggle('active');
+        this.classList.toggle('active');
+        overlay.classList.toggle('active');
     })
+
+    window.addEventListener('resize', function(){
+        mobMenu.classList.remove('active');
+        mobileMenuToggle.classList.remove('active');
+        overlay.classList.remove('active');
     })
+})
