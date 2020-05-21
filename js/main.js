c887675f047110782c9b372815afad817dfcd140
Anastasiya <zagoraai33@gmail.com>
2020-05-21 11:02:59 +0300

    portfolio21.05

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
     const mobileMenuToggle = document.querySelector('.toggle-menu');
     const mobMenu = document.querySelector('.header-menu');
-    const overlay = document.querySelector('#overlay')
+    const overlay = document.querySelector('#overlay');
+    const bodyEl = document.querySelector('body');
 
     mobileMenuToggle.addEventListener('click', function(){
         mobMenu.classList.toggle('active');
         this.classList.toggle('active');
         overlay.classList.toggle('active');
+        bodyEl.classList.toggle('active');
+
     })
 
     window.addEventListener('resize', function(){
         mobMenu.classList.remove('active');
         mobileMenuToggle.classList.remove('active');
         overlay.classList.remove('active');
+        bodyEl.classList.remove('active');
     })
 })
