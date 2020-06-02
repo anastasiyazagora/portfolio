f93f018426faddcc2730f156ebcf282cbb1b754b
Anastasiya <zagoraai33@gmail.com>
2020-06-02 16:12:47 +0300

    pravki02-06

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
+    // nav-page
+    $('#page-nav').onePageNav({
+        currentClass: 'active',
+        changeHash: false,
+        scrollSpeed: 750,
+        scrollThreshold: 0.5,
+        filter: '',
+        easing: 'swing',
+        begin: function () {},
+        end: function () {},
+        scrollChange: function ($currentListItem) {}
+    });
+
     const mobileMenuToggle = document.querySelector('.toggle-menu');
     const mobMenu = document.querySelector('.header-menu');
     const overlay = document.querySelector('#overlay');
